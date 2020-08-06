dc5399431745af551b94fa34e41017ba77e79667
Mikhail Koval <koval.mv@phystech.edu>
2020-08-06 10:43:41 +0300

    add cmakelist

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <SFML/Graphics.hpp>
+#include "lib/shapes.h"
+#include "lib/game.h"
+int main() {	
+	Game game;
+	game.start();
+}
--- tetris.cpp
+++
@@ -1,7 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include "libraries/shapes.h"
-#include "libraries/game.h"
-int main() {	
-	Game game;
-	game.start();
-}
